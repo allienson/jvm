dcdac88334b8dc18b2677849a0084b54da0935f2
luiz <luizfernandovieira_cf@hotmail.com>
2017-06-16 11:43:22 -0300

    add flags gcov

--- main.c
+++ main.c
@@ -13,12 +13,14 @@
 //
 //  Inicializa o programa, pegando o caminho do arquivo
 //  ".class" e disparando a execuacao do leitor. Após
-//  a leitura, executa a funcao de apresentacao das 
+//  a leitura, executa a funcao de apresentacao das
 //  estruturas lidas e formatadas.
 
-#include "exibidor.h" 
+#include "exibidor.h"
 #include "carregador.h"
-//#include "metodo.h" 
+//#include "metodo.h"
+
+//deletar
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,19 +31,19 @@ char* nomeArquivo;
 
 int main(int argc, char* argv[]) {
     nomeArquivo = calloc(TAMANHO_ARQUIVO, sizeof(argv[1]));
-    
+
     // Cria uma estrutura de execucao para chamar o Main da primeira classe carregada
     MethodInfo* Main;
-    
+
     // Verificacao para passagem de argumentos por linha de comando
     if (argc < 2) {
     	printf("\n\nInforme o caminho completo do arquivo \".class\" que contem o metodo main:\n");
     	scanf("%s", nomeArquivo);
     	getchar();
     } else {
-    	strcpy(nomeArquivo, argv[1]);	
+    	strcpy(nomeArquivo, argv[1]);
     }
-    
+
     // Carrega a classe java.lang.Object
     carregaObjectClass("java/lang/Object");
 
